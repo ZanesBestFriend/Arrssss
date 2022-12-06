b3cd5857d1ce0adb15f0baa1f2a88860a6dca193
ZanesBestFriend <niccostacey@gmail.com>
2022-12-06 09:16:35 -0600

    Updated Arrays

--- src/App.java
+++ src/App.java
@@ -11,11 +11,31 @@ public class App {
         return array;
     }
 
-    //Integer array method
+    //Integer array method(1D)
     public static int[] printArray(int[] array){
         for(int i = 0; i <array.length; i++){
             //array[i] = array[i].toUpperCase();
             System.out.println(array[i]);
+        }
+        return array;
+    }
+
+    //Integer array method (2D)
+    public static int[][] printArray(int[][] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[0].length; j++){
+                System.out.println(array[i][j]);
+            }
+        }
+        return array;
+    }
+    //String array method (2D)
+    public static String[][] printArray(String[][] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[0].length; j++){
+                System.out.print(array[i][j].toUpperCase());
+            }
+            System.out.println();
         }
         return array;
     }
@@ -32,25 +52,25 @@ public class App {
 
         // System.out.println(a[0]);
 
-        //String array
-        String coolPeeps[] = {"BUkey!", "Wu", "ZANE", "GUlanbenjamin", "chuck", "potato"};
+        //String array (1D)
+        // String coolPeeps[] = {"BUkey!", "Wu", "ZANE", "GUlanbenjamin", "chuck", "potato"};
 
         // printArray(coolPeeps);
 
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        String a[] = s.split(",");
+        // Scanner in = new Scanner(System.in);
+        // String s = in.nextLine();
+        // String a[] = s.split(",");
 
-        int intArray[] = new int[a.length];
+        // int intArray[] = new int[a.length];
 
-        for(int i = 0; i < intArray.length; i++){
-            intArray[i] = Integer.parseInt(a[i]);
-        }
-        printArray(intArray);
+        // for(int i = 0; i < intArray.length; i++){
+        //     intArray[i] = Integer.parseInt(a[i]);
+        // }
+        // printArray(intArray);
         
         // String stringArray[] = new String[5];
 
-        printArray(intArray);
+        // printArray(intArray);
         // printArray(stringArray);
         // printArray(a);
 
@@ -65,6 +85,28 @@ public class App {
         // b[0] = "Lek";
         // System.out.println(b[0]);
 
+        //2D Arrays
+            //int a[][] = {
+                //row 0: {1,2,3}
+                //row 1: {4,5,6}
+        //}
+        //1 = a[0][0] 2 = a[0][1] 3 = a[0][2]
+
+        int a[][] = {{1,2,3},{4,5,6}};
+
+        System.out.println(a[1][2]);
+
+        
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; j < a[i].length; j++){
+                a[i][j] *= a[i][j];
+            }
+        }
+        printArray(a);
+
+        String employees[][] = {{"William ", "Brady ", "Hand ", "$40K"},
+                                {"Kimory ", "Stanley ", "Janitor ", "$200k"}};
+        printArray(employees);
 
 
 
